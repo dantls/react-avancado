7db40af791da83802e6038ad904ee07409724903
Danilo Gomes <51554360+dantls@users.noreply.github.com>
2021-09-27 23:47:55 -0400

    Initial Class

---
+++ 005-primeiro-component/src/Input.tsx
@@ -0,0 +1,9 @@
+
+export function Input() {
+  return (
+    <div>
+      <label htmlFor="name">Nome</label>
+      <input id="name"type="text" />
+    </div>
+  )
+}
---
+++ 005-primeiro-component/src/main.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom'
+import './index.css'
+import { Input } from './Input'
+
+ReactDOM.render(
+  <Input />,
+  document.getElementById('root')
+)
